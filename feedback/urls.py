a9dea1c39a27dcb21b8b291fce8c00eab6ad9dd1
Valeriy Svetkin <svvaliv@gmail.com>
2022-06-30 11:45:43 +0500

    create FormView

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import DoneView, FeedBackView, FeedBackUpdateView, ListFeedBack, DetailFeedBack
+from .views import DoneView, FeedBackView, FeedbackUpdateView, ListFeedBack, DetailFeedBack
 
 urlpatterns = [
     path('done', DoneView.as_view()),
     path('list', ListFeedBack.as_view()),
-    path('detail/<int:id_feedback>', DetailFeedBack.as_view()),
+    path('detail/<int:pk>', DetailFeedBack.as_view()),
     path('', FeedBackView.as_view()),
-    path('<int:id_feedback>', FeedBackUpdateView.as_view()),
+    path('<int:id_feedback>', FeedbackUpdateView.as_view()),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 
 from .forms import FeedbackForm
 from .models import Feedback
@@ -10,24 +11,41 @@ from .models import Feedback
 
 # Create your views here.
 
-class FeedBackView(View):
-    def get(self, request):
-        form = FeedbackForm()
-        return render(request, "feedback/feedback.html", context={
-            "form": form
-        })
+# class FeedBackView(View):
+#     def get(self, request):
+#         form = FeedbackForm()
+#         return render(request, "feedback/feedback.html", context={
+#             "form": form
+#         })
+#
+#     def post(self, request):
+#         form = FeedbackForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/done")
+#         return render(request, "feedback/feedback.html", context={
+#             "form": form
+#         })
 
-    def post(self, request):
-        form = FeedbackForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/done")
-        return render(request, "feedback/feedback.html", context={
-            "form": form
-        })
+class FeedBackView(FormView):
+    form_class = FeedbackForm
+    template_name = "feedback/feedback.html"
+    success_url = "/done"
 
+    def form_valid(self, form):
+        form.save()
+        return super(FeedBackView, self).form_valid(form)
 
-class FeedBackUpdateView(View):
+    # def post(self, request):
+    #     form = FeedbackForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/done")
+    #     return render(request, "feedback/feedback.html", context={
+    #         "form": form
+    #     })
+
+class FeedbackUpdateView(View):
     def get(self, request, id_feedback):
         feed = Feedback.objects.get(id=id_feedback)
         form = FeedbackForm(instance=feed)
@@ -56,15 +74,6 @@ class DoneView(TemplateView):
         return context
 
 
-# class ListFeedBack(TemplateView):
-#     template_name = "feedback/list_feedback.html"
-#
-#     def get_context_data(self, **kwargs):
-#         feedbacks = Feedback.objects.all()
-#         context = super().get_context_data(**kwargs)
-#         context["feedbacks"] = feedbacks
-#         return context
-
 class ListFeedBack(ListView):
     template_name = "feedback/list_feedback.html"
     model = Feedback
@@ -72,16 +81,9 @@ class ListFeedBack(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        filter_qs = queryset.filter(rating__gt=4)
-        return filter_qs
+        return queryset
 
 
-class DetailFeedBack(TemplateView):
+class DetailFeedBack(DetailView):
     template_name = "feedback/detail_feedback.html"
-
-    def get_context_data(self, **kwargs):
-        feed = Feedback.objects.get(id=kwargs["id_feedback"])
-        # form = FeedbackForm(instance=feed)
-        context = super().get_context_data(**kwargs)
-        context["feed"] = feed
-        return context
+    model = Feedback
