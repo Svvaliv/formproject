cf5d0c5431ba3a0afb6520715506abddb2b31e1c
Valeriy Svetkin <svvaliv@gmail.com>
2022-06-30 16:17:55 +0500

    creating a CreateGalleryView class inherited from CreateView to output and send files from the form

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
 
 from .forms import GalleryUploadForm
 from .models import Gallery
@@ -9,15 +10,23 @@ from .models import Gallery
 # Create your views here.
 
 
-class GalleryView(View):
-    def get(self, request):
-        form = GalleryUploadForm
-        return render(request, "gallery/load_file.html", {"form": form})
+# class GalleryView(View):
+#     def get(self, request):
+#         form = GalleryUploadForm
+#         return render(request, "gallery/load_file.html", {"form": form})
+#
+#     def post(self, request):
+#         form = GalleryUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             new_image = Gallery(image=form.cleaned_data['image'])
+#             new_image.save()
+#             return HttpResponseRedirect('load_image')
+#         return render(request, "gallery/load_file.html", {"form": form})
 
-    def post(self, request):
-        form = GalleryUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_image = Gallery(image=form.cleaned_data['image'])
-            new_image.save()
-            return HttpResponseRedirect('load_image')
-        return render(request, "gallery/load_file.html", {"form": form})
+
+class CreateGalleryView(CreateView):
+    model = Gallery
+    fields = '__all__'
+    context_object_name = 'form'
+    template_name = 'gallery/load_file.html'
+    success_url = '/load_image'
