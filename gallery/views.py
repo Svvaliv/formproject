22dfea8bf1202b5a5f5b2d39b20467a37c2bde4f
Valeriy Svetkin <svvaliv@gmail.com>
2022-06-30 14:00:46 +0500

    implementation of the algorithm for uploading a file

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 
+
 # Create your views here.
+def storage_file(file):
+    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
 
 class GalleryView(View):
     def get(self, request):
         return render(request, "gallery/load_file.html")
 
-    def post(self, requese):
-        pass
+    def post(self, request):
+        storage_file(request.FILES['image'])
+        return render(request, "gallery/load_file.html")
