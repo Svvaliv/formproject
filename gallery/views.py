e4bd756cb900269af26753e34de0d9bd41f815d7
Valeriy Svetkin <svvaliv@gmail.com>
2022-06-30 15:24:47 +0500

    creating a model for saving files

--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+
 from .forms import GalleryUploadForm
-from django.http import HttpResponseRedirect
+from .models import Gallery
 
 
 # Create your views here.
-def storage_file(file):
-    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
-        for chunk in file.chunks():
-            new_file.write(chunk)
 
 
 class GalleryView(View):
@@ -19,6 +17,7 @@ class GalleryView(View):
     def post(self, request):
         form = GalleryUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            storage_file(request.FILES['image'])
+            new_image = Gallery(image=form.cleaned_data['image'])
+            new_image.save()
             return HttpResponseRedirect('load_image')
         return render(request, "gallery/load_file.html", {"form": form})
