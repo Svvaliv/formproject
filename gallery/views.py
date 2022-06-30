5ae9179a4b643e4c552533452f1231ce3712146c
Valeriy Svetkin <svvaliv@gmail.com>
2022-06-30 14:55:55 +0500

    file upload validation in the post method

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import GalleryUploadForm
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -11,8 +13,12 @@ def storage_file(file):
 
 class GalleryView(View):
     def get(self, request):
-        return render(request, "gallery/load_file.html")
+        form = GalleryUploadForm
+        return render(request, "gallery/load_file.html", {"form": form})
 
     def post(self, request):
-        storage_file(request.FILES['image'])
-        return render(request, "gallery/load_file.html")
+        form = GalleryUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            storage_file(request.FILES['image'])
+            return HttpResponseRedirect('load_image')
+        return render(request, "gallery/load_file.html", {"form": form})
